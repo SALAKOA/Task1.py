a612298ca9a23f4c9d5fbc943449bbdb24576c04
SALAKOA <salakoabdulrazaq@gmail.com>
2021-11-22 21:13:33 +0100

    here is group 2 assignent

--- ass.py
+++ ass.py
@@ -1,7 +1,1 @@
-#Checking if a name is the same as revers
-user_name=input('kindly insert your name here:').lower()
-if user_name==user_name[::-1]:
-    print(f'Hi{user_name}:your name is a palindrome')
-    
-else:
-    print(f'Hi{user_name},your is not a palindrome')
+
